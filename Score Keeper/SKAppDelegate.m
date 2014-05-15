6224243237226499cfd8d197c1ec4a4051addd4d
Joshua Howland <development@jkhowland.com>
2014-05-15 00:29:12 -0600

    Adds view controller and navigation controller

--- Score Keeper/SKAppDelegate.m
+++ Score Keeper/SKAppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "SKAppDelegate.h"
 
+#import "SKScoreViewController.h"
+
 @implementation SKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -15,6 +17,12 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+
+    SKScoreViewController *viewController = [SKScoreViewController new];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    self.window.rootViewController = navigationController;
+    
+
     [self.window makeKeyAndVisible];
     return YES;
 }
