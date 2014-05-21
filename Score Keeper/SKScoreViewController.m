eb3ab10fe605162f61ec6d97c61a7209eab0e351
Joshua Howland <development@jkhowland.com>
2014-05-21 01:10:39 -0600

    Adds add and remove buttons

--- Score Keeper/SKScoreViewController.m
+++ Score Keeper/SKScoreViewController.m
@@ -13,11 +13,16 @@ static CGFloat nameFieldWidth = 90;
 static CGFloat scoreFieldWidth = 60;
 static CGFloat stepperButtonWidth = 90;
 
+static CGFloat buttonWidth = 130;
+
 @interface SKScoreViewController () <UITextFieldDelegate>
 
 @property (nonatomic, strong) NSMutableArray *scores;
 @property (nonatomic, strong) NSMutableArray *scoreFields;
 @property (nonatomic, strong) UIScrollView *scrollView;
+
+@property (nonatomic, strong) UIButton *addButton;
+@property (nonatomic, strong) UIButton *removeButton;
 
 @end
 
@@ -39,10 +44,11 @@ static CGFloat stepperButtonWidth = 90;
     self.scores = [NSMutableArray new];
     self.scoreFields = [NSMutableArray new];
     
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    [self updateScrollViewContentSize];
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 64)];
     [self.view addSubview:scrollView];
-    
+    self.scrollView = scrollView;
+
+    [self updateScrollViewContentSize];
     [self setupInitialScoreViews];
     
 }
@@ -58,8 +64,6 @@ static CGFloat stepperButtonWidth = 90;
     NSInteger index = [self.scores count];
 
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, index * scoreHeight, self.view.frame.size.width, scoreHeight)];
-    view.backgroundColor = [UIColor lightGrayColor];
-    
     UITextField *nameField = [[UITextField alloc] initWithFrame:CGRectMake(20, 23, nameFieldWidth, 44)];
     nameField.delegate = self;
     nameField.placeholder = @"Name";
@@ -79,13 +83,54 @@ static CGFloat stepperButtonWidth = 90;
     [view addSubview:stepper];
     
     UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(0, scoreHeight - 1, self.view.frame.size.width, 1)];
-    separator.backgroundColor = [UIColor darkGrayColor];
+    separator.backgroundColor = [UIColor lightGrayColor];
     [view addSubview:separator];
     
     [self.scores addObject:view];
-    [self.view addSubview:view];
+    [self.scrollView addSubview:view];
     
+    [self updateButtonView];
+
 }
+
+- (void)updateButtonView {
+    
+    if (!self.addButton) {
+        UIButton *addButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [addButton setTitle:@"Add Player" forState:UIControlStateNormal];
+        [addButton addTarget:self action:@selector(addScoreView) forControlEvents:UIControlEventTouchUpInside];
+        [self.scrollView addSubview:addButton];
+        
+        self.addButton = addButton;
+    }
+    
+    self.addButton.frame = CGRectMake(20, ([self.scores count] * scoreHeight) + 23, buttonWidth, 44);
+    
+    if (!self.removeButton) {
+        
+        UIButton *removeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [removeButton setTitle:@"Remove Player" forState:UIControlStateNormal];
+        [removeButton addTarget:self action:@selector(removeLastScore) forControlEvents:UIControlEventTouchUpInside];
+        [self.scrollView addSubview:removeButton];
+        
+        self.removeButton = removeButton;
+    }
+    
+    self.removeButton.frame = CGRectMake(170, ([self.scores count] * scoreHeight) + 23, buttonWidth, 44);
+    
+    [self updateScrollViewContentSize];
+}
+
+- (void)removeLastScore {
+    
+    UIView *view = self.scores.lastObject;
+    [view removeFromSuperview];
+    
+    [self.scores removeObject:view];
+    
+    [self updateButtonView];
+}
+
 
 - (void)stepperChanged:(id)sender {
 
@@ -100,13 +145,13 @@ static CGFloat stepperButtonWidth = 90;
 
 - (void)updateScrollViewContentSize {
     
-    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, [self scrollViewHeight]);
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, [self scrollViewContentHeight]);
     
 }
 
-- (CGFloat)scrollViewHeight {
-    
-    return [self.scores count] * (scoreHeight + 1);
+- (CGFloat)scrollViewContentHeight {
+
+    return ([self.scores count] + 1) * (scoreHeight);
     
 }
 
