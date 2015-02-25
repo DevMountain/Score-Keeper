a772e8c45354835ce1e9488c27061e095070b3ad
Caleb Hicks <calebhicks@gmail.com>
2015-02-25 14:13:36 -0700

    cleans slate to blank project

---
+++ Score-Keeper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Score-Keeper
+//
+//  Created by Caleb Hicks on 2/25/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Score-Keeper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Score-Keeper
+//
+//  Created by Caleb Hicks on 2/25/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
